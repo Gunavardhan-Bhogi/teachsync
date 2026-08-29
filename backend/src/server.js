dda512054e7cfac728aea3cbfe74b655261a67f9
Gunavardhan-Bhogi <gunavardhanbhogi2003@gmail.com>
2026-08-29 23:51:25 +0530

    delpoyment setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,9 @@ await connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+	origin: '*'
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
